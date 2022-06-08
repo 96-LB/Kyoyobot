0ea3bc640420b22164588918892d9b433157e0fc
Lucas LaBuff <lucasbeef00@gmail.com>
2022-06-08 20:44:39 +0000

    dismantle TriggerManager

--- toes/triggers.py
+++ toes/triggers.py
@@ -80,57 +80,29 @@ class KeywordTrigger():
 
         return if_keyword(keyword)(with_probability(probability)((lambda x, y: '') if is_custom_emoji else trigger_reaction_standard(emoji_name)))
 
-class TriggerManager:
-    '''Stores and manages all trigger instances.'''
 
-    _triggers: Trigger = trigger_null()
-
-    @classmethod
-    def load_from_config(cls) -> None:
-        '''Loads all triggers from config.'''
-
-        TriggerManager._load_keyword_phrase_response_triggers_from_config()
-        TriggerManager._load_keyword_reaction_response_triggers_from_config()
-
-    @classmethod
-    def _load_keyword_phrase_response_triggers_from_config(cls) -> None:
-        '''Loads keyword_phrase_response triggers from config and adds them.'''
-
-        triggers: Dict = Config.get('triggers')
-        keyword_phrase_response_triggers = triggers.get('keyword_phrase_response', [])
-        for trigger_info in keyword_phrase_response_triggers:
-            TriggerManager.add_trigger(KeywordTrigger.from_phrase_response(**trigger_info))
-
-    @classmethod
-    def _load_keyword_reaction_response_triggers_from_config(cls) -> None:
-        '''Loads keyword_reaction_response triggers from config and adds them.'''
-
-        triggers: Dict = Config.get('triggers')
-        keyword_reaction_response_triggers = triggers.get('keyword_reaction_response', [])
-        for trigger_info in keyword_reaction_response_triggers:
-            TriggerManager.add_trigger(KeywordTrigger.from_reaction_response(**trigger_info))
-
-    @classmethod
-    def add_trigger(cls, trigger: Trigger) -> None:
-        '''Adds a trigger.'''
-
-        TriggerManager._triggers = after_trigger(TriggerManager._triggers)(trigger)
-
-    @classmethod
-    async def process_message_all(cls, bot: Client, message: Message) -> None:
-        '''Processes all triggers.'''
-
-        await cls._triggers(bot, message)
 
 def setup(bot: Client, tree: slash.CommandTree) -> None:
     '''Sets up this bot module.'''
 
-    TriggerManager.load_from_config()
+    trigger: Trigger = trigger_null()
+
+    trigger_config : Dict[str, str] = Config.get('triggers')
+
+    keyword_phrase_response_triggers = trigger_config.get('keyword_phrase_response', [])
+    for trigger_info in keyword_phrase_response_triggers:
+        trigger = after_trigger(trigger)(KeywordTrigger.from_phrase_response(**trigger_info))
+
+    keyword_reaction_response_triggers = trigger_config.get('keyword_reaction_response', [])
+    for trigger_info in keyword_reaction_response_triggers:
+        trigger = after_trigger(trigger)(KeywordTrigger.from_reaction_response(**trigger_info))
+
     
     @bot.event
     async def on_message(message: Message) -> None:
         # ignore messages sent by the bot (prevents potential infinite loops)
         if message.author == bot.user:
             return
-        if not DEBUG or message.guild.id == DEBUG_GUILD.id: #todo: change this
-            await TriggerManager.process_message_all(bot, message)
+        
+        if not DEBUG or (message.guild is not None and message.guild.id == DEBUG_GUILD.id): #todo: change this
+            await trigger(bot, message)
