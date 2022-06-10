aa1c069b810781cf52118f447c3d5d4160d19bbd
Sanketh Varamballi <sankethv03@gmail.com>
2022-06-09 23:23:50 -0700

    Added ask command. (#11)

--- toes/pinky.py
+++ toes/pinky.py
@@ -1,3 +1,5 @@
+from email import message
+import random
 from discord import app_commands as slash, Client, Interaction
 from util.debug import DEBUG_GUILD
 
@@ -9,7 +11,46 @@ async def ping(interaction: Interaction) -> None:
     
     await interaction.response.send_message('feet')
 
+@slash.command()
+async def ask(interaction: Interaction, question: str) -> None:
+    '''Ask a question to the all-knowing Kyoyobot.'''
+
+    # responses from: https://en.wikipedia.org/wiki/Magic_8_Ball#Possible_answers
+    responses = [
+        # Positive 
+        'It is certain.',
+        'It is decidedly so.',
+        'Without a doubt.',
+        'Yes definitely.',
+        'You may rely on it.',
+        'As I see it, yes.',
+        'Most likely.',
+        'Outlook good.',
+        'Yes.',
+        'Signs point to yes.',
+        # Neutral
+        'Reply hazy, try again.',
+        'Ask again later.',
+        'Better not tell you now.',
+        'Cannot predict now.',
+        'Concentrate and ask again.',
+        # Negative
+        'Don\'t count on it.',
+        'My reply is no.',
+        'My sources say no.',
+        'Outlook not so good.',
+        'Very doubtful.'
+    ]
+
+    text = (
+        f'Q: {question}\n'
+        f'A: **{random.choice(responses)}**'
+    )
+
+    await interaction.channel.send(text)
+
 def setup(bot: Client, tree: slash.CommandTree) -> None:
     '''Sets up this bot module.'''
 
     tree.add_command(ping, guild=(DEBUG_GUILD if DEBUG else None))
+    tree.add_command(ask, guild=(DEBUG_GUILD if DEBUG else None))
