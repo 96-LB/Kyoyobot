50deb4e633ee345306d0ec7e5b9da671e016d516
Lucas LaBuff <lucasbeef00@gmail.com>
2022-06-12 18:01:51 +0000

    remove the need for WordData

--- util/talk.py
+++ util/talk.py
@@ -1,45 +1,27 @@
-from typing import cast, Dict, List, Tuple
+from typing import cast, Dict, Iterator, List, Tuple
 from util.settings import TalkConfig
 import random
-
-class WordData:
-    '''Used by TalkGenerator to store data about word mappings.'''
-
-    def __init__(self, words: List[str], counts: List[int]) -> None:
-        '''Initializes WordData. Raises ValueError if words and counts aren't
-        the same length.'''
-        
-        if len(words) != len(counts):
-            raise ValueError("WordData :: words and counts must be of the same length.")
-
-        self.words = words
-        self.counts = counts
-
-    def choose_word(self) -> str:
-        '''Chooses a random word that will be the next word in the text.'''
-
-        ret = random.choices(self.words, weights=self.counts)
-        return ret[0]
 
 class TalkGenerator:
     '''Handles generating text that sounds like Kyoyo.'''
 
-    word_to_data: Dict[str, WordData] = {}
+    word_to_data: Dict[str, Tuple[Iterator[str], Iterator[int]]] = {}
 
     @classmethod
     def setup(cls) -> None:
         '''Sets up word_to_data using the configuration loaded by TalkConfig.'''
 
-        for from_word in TalkConfig.keys():
-            data = cast(Dict[str, Dict[str, int]], TalkConfig.get(from_word)).get('next_words')
-            assert data is not None
-            words_and_counts: List[Tuple[str, int]] = [(word, int(count)) \
-                for (word, count) in data.items()]
-            TalkGenerator.word_to_data[from_word] = WordData(
-                words=[word for (word, _) in words_and_counts],
-                counts=[count for (_, count) in words_and_counts],
-            )
+        for word in TalkConfig.keys():
+            data = cast(Dict[str, Dict[str, int]], cls[word])['next_words']         
+            cls.word_to_data[word] = tuple(zip(*data.items()))
 
+    @classmethod
+    def choose_word(cls, word: str) -> str:
+        '''Chooses a random word that will be the next word in the text.'''
+
+        words, counts = cls.word_to_data.get(word)
+        return random.choices(words, weights=counts)[0]
+    
     @classmethod
     def generate(cls) -> str:
         '''Generates text using the loaded data.'''
